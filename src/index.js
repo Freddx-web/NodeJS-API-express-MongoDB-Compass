90d9986baea0f960bc19308dd0ffe8067ad8e697
Fredd <66976840+Freddx-web@users.noreply.github.com>
2022-07-15 12:36:19 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 9000;
+
+// Routes
+app.get("/", (req, res) =>{
+	res.send('Welcome to mi API');
+});
+//Mongoose Connect
+mongoose
+
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connect to MongoDB Atlas"))
+    .catch((error) => console.error(error));
+
+// Server
+app.listen(port, () => console.log('server listening on part', port));
